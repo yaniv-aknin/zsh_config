133484a1f86faa6ccfd806dc07e32cfc5d0a3732
Yaniv Aknin <yaniv@aknin.name>
2011-09-07 11:40:07 +0300

    restructure directory and install to be more to my liking

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #! /bin/sh
 
-rsync -avP .zshrc ~/.zshrc
-rsync -avP --delete .zsh.d/ ~/.zsh.d/
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 
+cat > ~/.zshrc << EOF
+##### Zsh directory ##############################################
+ZSH_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+for zshrc_snipplet in \$ZSH_DIR/S* ; do
+    source \$zshrc_snipplet
+done
+
+SITE_CUSTOM_FILE=\$HOME/.zsh.site
+
+if [ -e \$SITE_CUSTOM_FILE ]; then
+    source \$SITE_CUSTOM_FILE
+fi
+EOF
