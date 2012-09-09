b36271b28ae4de6e37f9389e731e1f9a6bc3338e
Yaniv Aknin <yaniv@aknin.name>
2012-09-09 16:21:30 +0300

    install: more support for executables

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 
+ZSH_DIR="$( cd "$( dirname "$0" )" && pwd )"
 cat > ~/.zshrc << EOF
 ##### Zsh directory ##############################################
-ZSH_DIR="$( cd "$( dirname "$0" )" && pwd )"
+ZSH_DIR="$ZSH_DIR"
+
+export PATH="$ZSH_DIR/bin":"$PATH"
 
 for zshrc_snipplet in \$ZSH_DIR/S* ; do
     source \$zshrc_snipplet
@@ -16,3 +19,9 @@ if [ -e \$SITE_CUSTOM_FILE ]; then
     source \$SITE_CUSTOM_FILE
 fi
 EOF
+
+cd "$ZSH_DIR"/src
+for c_file in *.c; do
+    implicit_makefile="$(echo $c_file | sed -e 's/\.c$//')"
+    make "$implicit_makefile"
+done
