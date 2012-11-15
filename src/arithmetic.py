35c0bf5f20570e4d0fed525b75f978a5cbe1874c
Yaniv Aknin <yaniv@aknin.name>
2012-11-15 16:35:33 +0200

    arithmetic: refactor to prepare for more functions

--- src/arithmetic.py
+++ src/arithmetic.py
@@ -2,25 +2,46 @@
 
 from __future__ import print_function
 
+import os
 import sys
+from functools import partial
 
-total = 0
-count = 0
-for line in sys.stdin:
-    count += 1
+printerr = partial(print, file=sys.stderr)
+
+commands = {}
+def command(func):
+    commands[func.__name__] = func
+
+def yield_stdin_floats():
+    for offset, line in enumerate(sys.stdin):
+        try:
+            yield offset, float(line.strip())
+        except ValueError:
+            printerr("bad input on line %d: %r can't be casted to float" % (offset, line,))
+            sys.exit(1)
+
+@command
+def total():
+    print(sum(number for offset, number in yield_stdin_floats()))
+
+@command
+def avg():
+    total = 0
+    for count, number in yield_stdin_floats():
+        total += number
     try:
-        number = float(line.strip())
-    except ValueError:
-        print("bad input on line %d: %r can't be casted to float" % (count, line,), file=sys.stderr)
+        print
+        print(total / (count + 1))
+    except NameError:
+        printerr("no input")
         sys.exit(1)
-    total += number
 
-if not count:
-    print(0) # special case no input
-elif 'total' in sys.argv[0]:
-    print(total)
-elif 'avg' in sys.argv[0]:
-    print(total / count)
-else:
-    print("invoke me as 'total' or 'avg', not %r" % (sys.argv[0],))
-    sys.exit(2)
+def main(options):
+    print(options)
+
+if __name__ == '__main__':
+    basename = os.path.basename(sys.argv[0])
+    if basename not in commands:
+        printerr('invoke me as one of "%s", not %r' % (", ".join(commands), basename))
+        sys.exit(2)
+    commands[basename]()
